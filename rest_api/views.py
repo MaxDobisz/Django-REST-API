0d684588d4e29b2e6e08f3824bdcc7086a8efd2f
MaxDobisz <maciejdobisz@gmail.com>
2023-07-23 17:30:32 +0100

    add get all endpoint

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Person
+
 
 @api_view(['GET'])
 def index(request):
     return Response({"Abc":"get view"})
+
+@api_view(['GET'])
+def get_all(request):
+    all_persons = Person.objects.all()
+    return Response(all_persons)
+
 
