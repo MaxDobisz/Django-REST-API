8f33b5333459ac53ff2393960c4283beaf5f1d1f
MaxDobisz <maciejdobisz@gmail.com>
2023-07-23 17:47:10 +0100

    add Person modle serializer

--- rest_api/views.py
+++ rest_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Person
-
+from .serializer import PersonSerializer
 
 @api_view(['GET'])
 def index(request):
@@ -11,6 +11,7 @@ def index(request):
 @api_view(['GET'])
 def get_all(request):
     all_persons = Person.objects.all()
-    return Response(all_persons)
+    serializer = PersonSerializer(all_persons, many=True)
+    return Response(serializer.data)
 
 
