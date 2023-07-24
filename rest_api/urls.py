fb9422a96434b7dc84f2345e894556f541301422
MaxDobisz <maciejdobisz@gmail.com>
2023-07-24 22:23:37 +0100

    restrict the access to views with basic auth, add a loop and dynamic attribute assignment to PUT view

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('get-all', views.get_all),
     path('get-person', views.get_person),
     path('create-person', views.create_person),
     path('delete-person', views.delete_person),
--- rest_api/views.py
+++ rest_api/views.py
@@ -3,13 +3,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Person
 from .serializer import PersonSerializer
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def index(request):
-    return Response({"Abc":"get view"})
-
-@api_view(['GET'])
-def get_all(request):
     all_persons = Person.objects.all()
     serializer = PersonSerializer(all_persons, many=True)
     return Response(serializer.data)
@@ -25,6 +26,8 @@ def get_person(request):
         return Response({"ERROR":"The person dose not exist"}, status=404)
 
 @api_view(['GET', 'POST'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def create_person(request):
     data = request.data
     serializer = PersonSerializer(data=data)
@@ -35,6 +38,8 @@ def create_person(request):
         return Response(serializer.errors, status=400)
     
 @api_view(['Delete'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def delete_person(request):
     person_id = request.data.get('person_id')
     try:
@@ -45,39 +50,26 @@ def delete_person(request):
         return Response({"ERROR":"The person dose not exist"}, status=404)
     
 @api_view(['PUT'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
 def update_person(request):
     person_id = request.data.get('person_id')
-    get_new_first_name = request.data.get('new_first_name')
-    get_new_last_name = request.data.get('new_last_name')
-    get_new_email = request.data.get('new_email')
-    get_new_phone = request.data.get('new_phone')
-    get_new_dateOfBirth = request.data.get('new_dateOfBirth')
-    get_new_age = request.data.get('new_age')
-    get_new_username = request.data.get('new_username')
-    get_new_password = request.data.get('new_password')
 
     try:
         person = Person.objects.get(id=person_id)
 
-        if get_new_first_name:
-            person.first_name = get_new_first_name
-        if get_new_last_name:
-            person.last_name = get_new_last_name
-        if get_new_email:
-            person.email = get_new_email
-        if get_new_phone:
-            person.phone = get_new_phone
-        if get_new_dateOfBirth:
-            person.dateOfBirth = get_new_dateOfBirth
-        if get_new_age:
-            person.age = get_new_age
-        if get_new_username:
-            person.username = get_new_username
-        if get_new_password:
-            person.password = get_new_password
-        
+        fields_to_update = [
+            'first_name', 'last_name', 'email', 'phone',
+            'dateOfBirth', 'age', 'username', 'password'
+        ]
+
+        for field in fields_to_update:
+            new_value = request.data.get('new_' + field)
+            if new_value is not None:
+                setattr(person, field, new_value)
+
         person.save()
-        return Response({"SUCCESS":"The person has been updated"})
+        return Response({"SUCCESS": "The person has been updated"})
     
     except Person.DoesNotExist:
-        return Response({"ERROR":"The person dose not exist"}, status=404)  
+        return Response({"ERROR": "The person does not exist"}, status=404) 
